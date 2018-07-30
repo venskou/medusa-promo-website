8fc7720be462c5a16fa68061dc4dd8a76e836224
nicktabolich <nicktabolich@gmail.com>
2018-07-30 16:11:36 +0300

    v3

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -47,6 +47,7 @@ $(function () {
   });
   function setBackgroundPosition(){
     $('.typo__wrapper').css('background-position', "0px " + (Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 1.5) + "px");
+    $('.typo__quote').css('background-position', "right " + (Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 6) + "px");
   }
 
 
@@ -466,38 +467,40 @@ $(document).ready(function () {
 
   $('.sw-btn-next').text('Continue');
 
-  var deadline = 'Jul 25 2019 18:40:18 GMT-0400';
+  if ($('#counter').length) {
+    var deadline = 'Jul 25 2019 18:40:18 GMT-0400';
 
-  function time_remaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor( (t/1000) % 60 );
-    var minutes = Math.floor( (t/1000/60) % 60 );
-    var hours = Math.floor( (t/(1000*60*60)) % 24 );
-    var days = Math.floor( t/(1000*60*60*24) );
-    return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
-  };
-
-  function run_clock(id,endtime) {
-    var clock = document.getElementById(id);
-    var days_span = clock.querySelector('.counter__valueText--days');
-    var hours_span = clock.querySelector('.counter__valueText--hours');
-    var minutes_span = clock.querySelector('.counter__valueText--minutes');
-    var seconds_span = clock.querySelector('.counter__valueText--seconds');
-
-    function update_clock() {
-      var t = time_remaining(endtime);
-      days_span.innerHTML = t.days;
-      hours_span.innerHTML = ('0' + t.hours).slice(-2);
-      minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
-      seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
-      if(t.total<=0){ clearInterval(timeinterval); }
+    function time_remaining(endtime) {
+      var t = Date.parse(endtime) - Date.parse(new Date());
+      var seconds = Math.floor( (t/1000) % 60 );
+      var minutes = Math.floor( (t/1000/60) % 60 );
+      var hours = Math.floor( (t/(1000*60*60)) % 24 );
+      var days = Math.floor( t/(1000*60*60*24) );
+      return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
     };
 
-    update_clock();
-    var timeinterval = setInterval(update_clock,1000);
-  };
+    function run_clock(id,endtime) {
+      var clock = document.getElementById(id);
+      var days_span = clock.querySelector('.counter__valueText--days');
+      var hours_span = clock.querySelector('.counter__valueText--hours');
+      var minutes_span = clock.querySelector('.counter__valueText--minutes');
+      var seconds_span = clock.querySelector('.counter__valueText--seconds');
 
-  run_clock('counter',deadline);
+      function update_clock() {
+        var t = time_remaining(endtime);
+        days_span.innerHTML = t.days;
+        hours_span.innerHTML = ('0' + t.hours).slice(-2);
+        minutes_span.innerHTML = ('0' + t.minutes).slice(-2);
+        seconds_span.innerHTML = ('0' + t.seconds).slice(-2);
+        if(t.total<=0){ clearInterval(timeinterval); }
+      };
+
+      update_clock();
+      var timeinterval = setInterval(update_clock,1000);
+    };
+
+    run_clock('counter',deadline);
+  }
 
   $(window).resize(function() {
     $('.registration__stepContentWrapper').each(function(i) {
